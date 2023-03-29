ede2a127aeef374b741391bf2443731202d563ae
Jaesung Ryu <cwryu6252@naver.com>
2023-03-29 22:53:24 +0900

    4.4 widget lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,8 +35,10 @@ class _AppState extends State<App> {
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            MyLargeTitle(),
+          children: [
+            showTitle ? const MyLargeTitle() : const Text("nothing"),
+            IconButton(
+                onPressed: toggleTitle, icon: const Icon(Icons.remove_red_eye))
           ],
         )),
       ),
@@ -36,10 +46,23 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('Hello!');
+  }
 
   @override
   Widget build(BuildContext context) {
