77b70aa005ec590a2b2c30d7b35fc2fbefee6fe7
Jaesung Ryu <cwryu6252@naver.com>
2023-04-01 10:54:50 +0900

    6.9 detail screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/widgets/webtoon_widgets.dart';
 
 import '../services/api_services.dart';
 
@@ -50,46 +51,13 @@ class HomeScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    blurRadius: 3,
-                    offset: const Offset(10, 10),
-                  ),
-                ],
-              ),
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              child: imageNetwork(webtoon),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
         );
       },
       separatorBuilder: (context, index) => const SizedBox(width: 40),
-    );
-  }
-
-  Image imageNetwork(WebtoonModel webtoon) {
-    return Image.network(
-      webtoon.thumb,
-      headers: const {
-        "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-      },
     );
   }
 }
