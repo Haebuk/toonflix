ae4b1c100b59eceac6ed7b0c1ecd28451090a05d
Jaesung Ryu <cwryu6252@naver.com>
2023-04-01 11:15:02 +0900

    api services

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -19,6 +21,30 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLastEpisodeById(String id) async {
+    List<WebtoonEpisodeModel> episodeInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<Map<String, dynamic>> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodeInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodeInstances;
     }
     throw Error();
   }
